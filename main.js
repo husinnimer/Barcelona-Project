9f359547ed1a3dcc08defa838e079aa365da4b9f
husinnimer <husin2001n@gmail.com>
2024-05-21 17:04:29 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let section3 = document.querySelector(".services");
+let cont = document.querySelector(".boxes");
+
+let section2 = document.querySelector(".service-desighn");
+let image = document.querySelector(".image img");
+let box = document.querySelector(".text-desighn");
+
+let section = document.querySelector(".information");
+let spans = document.querySelectorAll(".prog span");
+
+let secion4 = document.querySelector(".price");
+
+window.onscroll = function () {
+  if (window.scrollY >= section3.offsetTop - 400) {
+    cont.style.transform = "scale(1)";
+  }
+
+  if (window.scrollY >= section2.offsetTop - 200) {
+    image.style.bottom = "0";
+    box.style.flex = "1";
+  }
+
+  if (window.scrollY >= section.offsetTop - 200)
+    spans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+
+  if (window.scrollY >= secion4.offsetTop - 400) {
+    secion4.style.transform = "scale(1)";
+  }
+};
